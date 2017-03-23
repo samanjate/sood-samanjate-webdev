8b7fc9bf5d4934da00c2f430316e7b943f946ef7
samanjate <samanjate@gmail.com>
2017-03-23 01:01:40 -0400

    fixed bug

--- public/assignment/views/widget/controllers/widget-flickr-search.controller.client.js
+++ public/assignment/views/widget/controllers/widget-flickr-search.controller.client.js
@@ -27,12 +27,12 @@
             var url = "https://farm" + photo.farm + ".staticflickr.com/" + photo.server;
             url += "/" + photo.id + "_" + photo.secret + "_b.jpg";
             WidgetService
-                .findWidgetById(vm.wgid)
+                .findWidgetById(vm.uid, vm.wid, vm.pid, vm.wgid)
                 .then(function (response) {
                     var updatedWidget = response.data;
                     updatedWidget.url = url;
                     WidgetService
-                        .updateWidget(vm.wgid, updatedWidget)
+                        .updateWidget(vm.uid, vm.wid, vm.pid, vm.wgid, updatedWidget)
                         .then(function (response) {
                             var updatedWidgetObject = response;
                             if(updatedWidgetObject){
@@ -42,6 +42,7 @@
                             vm.updateerror = "Could not update the widget!";
                         });
                 }, function (err) {
+                    console.log("success");
                     vm.updateerror = "Could not get the widget!";
                 });
         }
